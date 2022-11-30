fd2b55ffe999296406b11b27d241096e1e2e6ad9
Cordre <cordrepayne@gmail.com>
2022-11-30 12:58:11 -0600

    add text lines to openMenu and switch statement for user input

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 import java.util.Scanner; //scanner used for gathering user input in command line
 
+//keys
+// something.equals(something)
+// scanner.close()
+// String value = scanner.next();
+// value = scanner.next().toLowerCase();
+// String[] choices = {rock, paper, scissors}
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
@@ -9,17 +15,29 @@ public class App {
             openMenu(running, scanner);
         }
         //text
-        System.out.println("Welcome To Cordre's Premium Banking Services");
         System.out.println("Would you like to view your current balance?(v)");
         System.out.println("Current Balance is: ");
-        System.out.println("Deposit balance(d)");
-        System.out.println("Withdraw balance(w)");
         System.out.println("Exchange currencies(e)");
         System.out.println("Available currencies are: ");
         System.out.println("Go back(b)");
         System.out.println("Quit(q)");
     };
-    public static void openMenu(boolean running, Scanner scanner){};
+    public static void openMenu(boolean running, Scanner scanner){
+        System.out.println("Welcome To Cordre's Premium Banking Services");
+        System.out.println("Would you like to view your current balance?(v)");
+        System.out.println("Deposit balance(d)");
+        System.out.println("Withdraw balance(w)");
+        String value = scanner.next().toLowerCase();
+        switch(value){
+            case "v":  System.out.println("view balance");
+            break;
+            case "b": System.out.println("deposit balance");
+            break;
+            case "w": System.out.println("withdraw balance");
+            break;
+            default: System.out.println("Error");
+        }
+    };
     public static void viewBalance(){};
     public static void withdrawBalance(){};
     public static void depositBalance(){};
